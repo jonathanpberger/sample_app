242beae0292d15df1dde34b9ec77d5cb63d037cf
jonathanpberger <jonathanpberger@gmail.com>
2010-06-06 14:57:10 -0400

    3.2 add test for about page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe PagesController do
 
-  #Delete these examples and add some real ones
-  it "should use PagesController" do
-    controller.should be_an_instance_of(PagesController)
-  end
-
-
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -18,6 +12,13 @@ describe PagesController do
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
       response.should be_success
     end
   end

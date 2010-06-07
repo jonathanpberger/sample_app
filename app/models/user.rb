eb2ea8d90788b8f76faca95c218543db4d2077e1
jonathanpberger <jonathanpberger@gmail.com>
2010-06-06 21:10:07 -0400

    6.2.1 add validates_presence_of name, email to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessible :name, :email
+  validates_presence_of :name, :email
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,13 +14,21 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @valid_attributes = {
+    @attr = {
       :name => "value for name",
       :email => "value for email"
     }
   end
 
   it "should create a new instance given valid attributes" do
-    User.create!(@valid_attributes)
+    User.create!(@attr)
+  end
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name  => ""))
+    no_name_user.should_not be_valid
+  end
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email  => ""))
+    no_email_user.should_not be_valid
   end
 end

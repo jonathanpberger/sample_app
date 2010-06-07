e340a787fe1302db8fa932fdb600eb29c06f04e9
jonathanpberger <jonathanpberger@gmail.com>
2010-06-06 21:40:15 -0400

    6.2.4 Uniqueness validation WIP

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,8 @@ require 'spec_helper'
 describe User do
   before(:each) do
     @attr = {
-      :name => "value for name",
-      :email => "value for email"
+      :name => "foo bar",
+      :email => "user@foo.com"
     }
   end
 
@@ -31,4 +31,41 @@ describe User do
     no_email_user = User.new(@attr.merge(:email  => ""))
     no_email_user.should_not be_valid
   end
+  
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name  => long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should accept valid email addys" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email  => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addys" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email  => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+  
+  it "should reject dupe email addys" do
+    User.create!(@attr)
+    user_with_dupe_email = User.new(@attr)
+    user_with_dupe_email.should_not be_valid
+  end
+
+  it "should reject email addys identical up to case" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email  => upcased_email))
+    user_with_dupe_email = User.new(@attr)
+    user_with_dupe_email.should_not be_valid
+    
+  end
+
 end
